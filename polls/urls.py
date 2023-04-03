6ec596d3ac413696fba5dfa11898e889282e12d7
503282404 <stanislav.kryshchuk@ge.com>
2023-04-03 15:03:50 +0300

    part4

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,19 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
+
+## func views
+#urlpatterns = [
+#    path('', views.index, name='index'),
+#    path('<int:qu_id>/', views.detail, name='detail'),
+#    path('<int:qu_id>/results/', views.results, name='results'),
+#    path('<int:qu_id>/vote/', views.vote, name='vote')
+#]
+
+# class views
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:qu_id>/', views.detail, name='detail'),
-    path('<int:qu_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:qu_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,59 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from pprint import pprint as pp
+from django.http import HttpResponseRedirect, Http404
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
+# func views
 
-def index(request):
-    print(f'{request=}\n{type(request)=}')
-    pp(request.__dict__)
-    latest_qu_list = Question.objects.order_by('-pub_date')[:5]
-#    templ = loader.get_template('polls/index.html')
-    context = {'latest_qu_list': latest_qu_list}
-#    return HttpResponse(', '.join([q.qu_text for q in latest_qu_list]))
-#    return HttpResponse(templ.render(context, request))
-    return render(request, 'polls/index.html', context)
-
-
-def detail(request, qu_id):
-    print(f'{request=}\n{type(request)=}')
-    pp(request.__dict__)
-
-    try:
-        qu = Question.objects.get(pk=qu_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
-
-    # or
-    # qu = get_object_or_404(Question, qu_text='Just added')
-
-    return render(request, 'polls/detail.html', {'qu': qu})
-
-
-def results(request, qu_id):
-    print(f'{request=}\n{type(request)=}')
-    pp(request.__dict__)
-
-    try:
-        qu = Question.objects.get(id=qu_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
-
-    return render(request, 'polls/results.html', {'qu': qu})
+#def index(request):
+#    latest_qu_list = Question.objects.order_by('-pub_date')[:5]
+##    templ = loader.get_template('polls/index.html')
+#    context = {'latest_qu_list': latest_qu_list}
+##    return HttpResponse(', '.join([q.qu_text for q in latest_qu_list]))
+##    return HttpResponse(templ.render(context, request))
+#    return render(request, 'polls/index.html', context)
+#
+#
+#def detail(request, qu_id):
+#    try:
+#        qu = Question.objects.get(pk=qu_id)
+#    except Question.DoesNotExist:
+#        raise Http404('Question does not exist')
+#
+#    # or
+#    # qu = get_object_or_404(Question, qu_text='Just added')
+#
+#    return render(request, 'polls/detail.html', {'qu': qu})
+#
+#
+#def results(request, qu_id):
+#    try:
+#        qu = Question.objects.get(id=qu_id)
+#    except Question.DoesNotExist:
+#        raise Http404('Question does not exist')
+#
+#    return render(request, 'polls/results.html', {'qu': qu})
 
 
 def vote(request, qu_id: int):
-    print(f'{request=}\n{type(request)=}')
-    pp(request.__dict__)
-    
     try:
         qu = Question.objects.get(id=qu_id)
     except Question.DoesNotExist:
         raise Http404('Question does not exist')
 
     try:
-        pp(f'{request.POST=}')
         selected_ch = qu.choice_set.get(id=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the qu voting form
@@ -69,3 +58,23 @@ def vote(request, qu_id: int):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back btn
         return HttpResponseRedirect(reverse('polls:results', args=(qu.id, )))
+
+
+# class views
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_qu_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
