3d52ea2844fd7e37bd0a055606ef41ea0de83c7b
503282404 <stanislav.kryshchuk@ge.com>
2023-04-05 08:13:18 +0300

    part5

--- polls/tests.py
+++ polls/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from datetime import timedelta
 from django.utils import timezone
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
 
 
 class QuestionModelTests(TestCase):
@@ -18,3 +19,65 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - timedelta(hours=23, minutes=59, seconds=59)
         recent_qu = Question(pub_date=time)
         self.assertIs(recent_qu.was_published_recently(), True)
+
+
+def create_qu(qu_text, days):
+    time = timezone.now() + timedelta(days=days)
+    return Question.objects.create(qu_text=qu_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_qu(self):
+        resp = self.client.get(reverse('polls:index'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'No polls are available')
+        self.assertQuerysetEqual(resp.context['latest_qu_list'], [])
+    
+    def test_past_qu(self):
+        qu = create_qu(qu_text='Past qu.', days=-30)
+        resp = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(resp.context['latest_qu_list'], [qu])
+    
+    def test_future_qu(self):
+        create_qu(qu_text='Future qu.', days=30)
+        resp = self.client.get(reverse('polls:index'))
+        self.assertContains(resp, 'No polls are available')
+        self.assertQuerysetEqual(resp.context['latest_qu_list'], [])
+
+    def test_futureandpast_qu(self):
+        qu = create_qu(qu_text='Past qu', days=-30)
+        create_qu(qu_text='Future qu', days=30)
+        resp = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(resp.context['latest_qu_list'], [qu])
+
+    def test_two_past_qus(self):
+        qu1 = create_qu(qu_text='Past qu 1', days=-30)
+        qu2 = create_qu(qu_text='Past qu 2', days=-20)
+        resp = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(resp.context['latest_qu_list'], [qu2, qu1])
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        fq = create_qu(qu_text='Future question', days=5)
+        resp = self.client.get(reverse('polls:detail', args=(fq.id,)))
+        self.assertEqual(resp.status_code, 404)
+
+    def test_past_question(self):
+        pq = create_qu(qu_text='Past qu', days=-5)
+        resp = self.client.get(reverse('polls:detail', args=(pq.id,)))
+        self.assertContains(resp, pq.qu_text)
+
+
+class QuestionResultsViewTests(TestCase):
+    def test_future_qu(self):
+        fq = create_qu(qu_text='Future qu', days=5)
+        Choice.objects.create(qu_id=fq.id, ch_text='Not much', votes=3)
+        resp = self.client.get(reverse('polls:results', args=(fq.id,)))
+        self.assertEqual(resp.status_code, 404)
+
+    def test_past_qu(self):
+        pq = create_qu(qu_text='Past qu', days=-5)
+        ch = Choice.objects.create(qu_id=pq.id, ch_text='Good choice', votes=10)
+        resp = self.client.get(reverse('polls:results', args=(pq.id,)))
+        self.assertContains(resp, ch.ch_text)
